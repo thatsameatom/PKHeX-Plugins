72ad6fdbc58463f3e139f26161aad52df41a250a
Omni-KingZeno <200784099+Omni-KingZeno@users.noreply.github.com>
2025-12-17 14:14:06 -0500

    Simplified Batch Commands

--- PKHeX.Core.AutoMod/AutoMod/Regeneration/RegenSet.cs
+++ PKHeX.Core.AutoMod/AutoMod/Regeneration/RegenSet.cs
@@ -82,6 +82,12 @@ public sealed class RegenSet
             if (line.Type == BattleTemplateParseErrorType.TokenUnknown)
             {
                 var sanitized = line.Value.Replace(">=", "≥").Replace("<=", "≤");
+
+                if (TryConvertToBatchFormat(sanitized, out var converted))
+                {
+                    sanitized = converted;
+                }
+
                 if (StringInstruction.TryParseInstruction(sanitized, out var mod))
                 {
                     mods.Add(mod);
@@ -114,6 +120,93 @@ public sealed class RegenSet
         EncounterFilters = eFilter;
         VersionFilters = vFilter;
         SeedFilters = sFilter;
+    }
+
+    private static bool TryConvertToBatchFormat(string input, [NotNullWhen(true)] out string? converted)
+    {
+        converted = null;
+
+        var colonIndex = input.IndexOf(':');
+        if (colonIndex <= 0 || colonIndex >= input.Length - 1)
+            return false;
+
+        if (input[0] is '.' or '=')
+            return false;
+
+        var keySpan = input.AsSpan(0, colonIndex).Trim();
+        var valueSpan = input.AsSpan(colonIndex + 1).Trim();
+
+        if (keySpan.IsEmpty || valueSpan.IsEmpty)
+            return false;
+
+        var key = ConvertKey(keySpan.ToString());
+        var value = valueSpan.ToString();
+
+        if (key == "RibbonMark")
+        {
+            converted = $".RibbonMark{value}=true";
+            return true;
+        }
+
+        var convertedValue = ConvertValue(key, value.ToLower());
+        converted = $".{key}={convertedValue}";
+        return true;
+    }
+
+    private static string ConvertKey(string key) => key switch
+    {
+        "Height" => "HeightScalar",
+        "HyperTrained" => "HyperTrainFlags",
+        "Size" => "Scale",
+        "Weight" => "WeightScalar",
+        "Mark" => "RibbonMark",
+        "Sweet" => "FormArgument",
+        _ => key
+    };
+
+    private static string ConvertValue(string key, string value)
+    {
+        return value switch
+        {
+            "strawberry" => "0",
+            "berry" => "1",
+            "love" => "2",
+            "star" => "3",
+            "clover" => "4",
+            "flower" => "5",
+            "ribbon" => "6",
+            "all" => "$suggestAll",
+            "none" => "$suggestNone",
+            "suggest" => "$suggest",
+            "yes" => key == "HyperTrainFlags" ? "1" : "true",
+            "no" => key == "HyperTrainFlags" ? "0" : "false",
+            _ when key is "Scale" or "WeightScalar" or "HeightScalar" => ConvertScalarValue(key, value),        
+            _ => value
+        };
+    }
+
+    private static string ConvertScalarValue(string key, string value)
+    {
+        var rnd = new Random();
+
+        return (key, value) switch
+        {
+            ("Scale", "xxxs") => "0",
+            ("Scale", "xxs") => $"{rnd.Next(1, 31)}",
+            ("Scale", "xs") => $"{rnd.Next(31, 61)}",
+            ("Scale", "s") => $"{rnd.Next(61, 101)}",
+            ("Scale", "av") => $"{rnd.Next(101, 161)}",
+            ("Scale", "l") => $"{rnd.Next(161, 196)}",
+            ("Scale", "xl") => $"{rnd.Next(196, 242)}",
+            ("Scale", "xxl") => $"{rnd.Next(242, 255)}",
+            ("Scale", "xxxl") => "255",
+            (_, "xs") => $"{rnd.Next(0, 16)}",
+            (_, "s") => $"{rnd.Next(16, 48)}",
+            (_, "av") => $"{rnd.Next(48, 208)}",
+            (_, "l") => $"{rnd.Next(208, 240)}",
+            (_, "xl") => $"{rnd.Next(240, 256)}",
+            _ => value
+        };
     }
 
     public string GetSummary()
